cb95b31b0c1cd96534e118a0f7d1a12c01eb86e8
rodrigo.r.rivas@gmail.com <rodrigo.r.rivas@gmail.com@cca8a690-92ae-11de-87e2-8d177609176d>
2009-09-03 18:31:45 +0000

    Metodos estaticos con texto a dividir como parametro. Nuevo metodo con pattern como otro parametro por si se quiere dividir de otra forma.

--- AODBuilder/src/util/TextSplitter.java
+++ AODBuilder/src/util/TextSplitter.java
@@ -1,36 +1,21 @@
-
 package util;
 
 import java.util.regex.Pattern;
 
 public class TextSplitter {
-	private String text;
 
+	public static String COMMON_PATTERN = "[';.,\\s]+";
 
-	public TextSplitter () {
-		super();
+	public static String [] split (String text) {
+		Pattern splitter = Pattern.compile(COMMON_PATTERN);
+		String [] result = splitter.split(text);
+		return result;
 	}
 
-	public String [] split () {
-		String pattern = "[';.,\\s]+";
+	public static String [] split (String text, String pattern) {
 		Pattern splitter = Pattern.compile(pattern);
 		String [] result = splitter.split(text);
 		return result;
 	}
 
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public static void print (String [] text) {
-		for (int i=0; i<text.length; i++)
-			System.out.println(text[i]);
-	}
-	public static void main(String[] args) {
-		TextSplitter textSplitter = new TextSplitter();
-		String text = "En 'Es imposible', su sexto álbum, la banda de Ale Sergi busca sonar más rockera y orgánica. La imagen es más madura; las letras, adolescentes como siempre"; 
-		textSplitter.setText(text);
-		String [] resultado = textSplitter.split();
-		print(resultado);
-	}
 }
